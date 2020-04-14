81735cb4248f125a8985fe0ef144ccafee24f5d7
PhakineeS <PhakineeS@github.com>
2020-04-14 13:16:35 +0700

    mini hw

---
+++ Function.py
@@ -0,0 +1,16 @@
+def minus2(x,y):
+    if x>=y:
+        print(x-y)
+    else:
+        print(y-x)
+
+def mul2(x,y):
+    print(x*y)
+
+def divide2(x,y):
+    print(x/y)
+'''
+minus2(3,2)
+mul2(3,2)
+divide2(3,2)
+'''
